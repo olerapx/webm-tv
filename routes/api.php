3a4db43b93c5c1df1104df7e3eb4afdd151b0598
olerapx <olerapx0@gmail.com>
2023-01-23 23:27:46 +0300

    Fix ajax auth

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,5 @@ Route::controller(\App\Http\Controllers\VideoController::class)->group(function 
 
 Route::controller(\App\Http\Controllers\Auth\UserController::class)->group(function () {
     Route::post('/login', 'login')
-        ->middleware(\Illuminate\Session\Middleware\StartSession::class)
         ->name('login');
 });
