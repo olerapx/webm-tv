6309e665cd4026bda8f307cf4bb82154cb8a6b22
olerapx <olerapx0@gmail.com>
2023-01-23 23:27:47 +0300

    Revert "Comment out login"
    This reverts commit 581bbf48eb9a565c8cba32adbacfca642831459d.

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -9,37 +9,37 @@
                 </div>
             </div>
 
-{{--            <div class="flex items-center">--}}
-{{--                @auth--}}
-{{--                    <x-dropdown align="right" width="48">--}}
-{{--                        <x-slot name="trigger">--}}
-{{--                            <button class="inline-flex items-center px-3 py-2 border border-transparent leading-4 font-medium rounded-md bg-white hover:text-gray-800 focus:outline-none transition ease-in-out duration-150">--}}
-{{--                                <div>{{ Auth::user()->name }}</div>--}}
+            <div class="flex items-center">
+                @auth
+                    <x-dropdown align="right" width="48">
+                        <x-slot name="trigger">
+                            <button class="inline-flex items-center px-3 py-2 border border-transparent leading-4 font-medium rounded-md bg-white hover:text-gray-800 focus:outline-none transition ease-in-out duration-150">
+                                <div>{{ Auth::user()->name }}</div>
 
-{{--                                <div class="ml-1">--}}
-{{--                                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">--}}
-{{--                                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />--}}
-{{--                                    </svg>--}}
-{{--                                </div>--}}
-{{--                            </button>--}}
-{{--                        </x-slot>--}}
+                                <div class="ml-1">
+                                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                    </svg>
+                                </div>
+                            </button>
+                        </x-slot>
 
-{{--                        <x-slot name="content">--}}
-{{--                            <x-dropdown-link :href="route('me')">{{ __('Me') }}</x-dropdown-link>--}}
-{{--                            <form method="POST" action="{{ route('logout') }}">--}}
-{{--                                @csrf--}}
-{{--                                <x-dropdown-link :href="route('logout')" onclick="event.preventDefault(); this.closest('form').submit();">{{ __('Log Out') }}</x-dropdown-link>--}}
-{{--                            </form>--}}
-{{--                        </x-slot>--}}
-{{--                    </x-dropdown>--}}
-{{--                @endauth--}}
+                        <x-slot name="content">
+                            <x-dropdown-link :href="route('me')">{{ __('Me') }}</x-dropdown-link>
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+                                <x-dropdown-link :href="route('logout')" onclick="event.preventDefault(); this.closest('form').submit();">{{ __('Log Out') }}</x-dropdown-link>
+                            </form>
+                        </x-slot>
+                    </x-dropdown>
+                @endauth
 
-{{--                @guest--}}
-{{--                    <div align="right">--}}
-{{--                        <a x-on:click.prevent="showLogin = true" href="" class="underline">Sign In</a>--}}
-{{--                    </div>--}}
-{{--                @endguest--}}
-{{--            </div>--}}
+                @guest
+                    <div align="right">
+                        <a x-on:click.prevent="showLogin = true" href="" class="underline">Sign In</a>
+                    </div>
+                @endguest
+            </div>
         </div>
     </div>
 
