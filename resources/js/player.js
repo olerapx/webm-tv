603b262ededdb24b27e6d5ba827aa0306d30e2b0
olerapx <olerapx0@gmail.com>
2023-01-23 23:27:46 +0300

    Prevent recursion

--- resources/js/player.js
+++ resources/js/player.js
@@ -39,7 +39,7 @@ class Player {
     }
 
     async _loadVideos(count) {
-        if (this.loading) {
+        if (this.loading || this.videoFetcher.noMoreVideos) {
             return;
         }
         this.loading = true;
