41dd478cc0b637f53180546a0e8ae87e978e9ae8
olerapx <olerapx0@gmail.com>
2023-01-29 21:24:18 +0300

    Replace hairsp with thin nbsp

--- resources/views/components/page-title.blade.php
+++ resources/views/components/page-title.blade.php
@@ -1,3 +1,3 @@
-<div class="absolute top-0 w-auto text-4xl text-center items-center font-semibold text-gray-900 pt-2" style="left: 50%; transform: translate(-50%)">
-    @foreach($breadcrumbs->generate() as $item)/&hairsp;<a class="underline" href=" {{$item['url']}} ">{{ $item['title'] }}</a>&hairsp;@endforeach
+<div class="absolute top-0 w-auto text-4xl text-center items-center font-semibold text-gray-900 pt-2 break-keep" style="left: 50%; transform: translate(-50%)">
+    @foreach($breadcrumbs->generate() as $item)/&#8239;<a class="underline" href=" {{$item['url']}} ">{{ $item['title'] }}</a>&#8239;@endforeach
 </div>
