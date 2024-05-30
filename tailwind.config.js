2957531394c7f5ea0c7a92f9793580e0c8ca3d72
olerapx <olerapx0@gmail.com>
2024-05-30 17:35:33 +0300

    Update to laravel 11

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,5 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
-
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
@@ -11,7 +9,7 @@ module.exports = {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+                sans: ['Nunito', 'sans-serif'],
             },
         },
     },
